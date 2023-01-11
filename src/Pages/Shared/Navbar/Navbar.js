d290d5a3c75de2e15531c9e8992b17eb997a91b8
nazrul-islam <nazrulislamrongon@gmail.com>
2023-01-11 12:52:33 +0600

    Navbar created

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Navbar = () => {
     return (
         <nav>
-            <h2>This is navbar</h2>
+            <h2>This is navbar section</h2>
         </nav>
     );
 };
