9affcad95cbdb35173888ad5dccd2f978802fab7
SkPipul <skjabed252@gmail.com>
2023-01-11 12:31:56 +0600

    main folder added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from './Banner/Banner';
 
 const Home = () => {
     return (
         <main>
-            <h1>This is home</h1>
+            <Banner></Banner>
         </main>
     );
 };
