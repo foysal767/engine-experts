e67d0993ac7f4e8082b51b1f3f8f1842ce555eb4
hridoyhalder1 <hridoyhalder91@gmail.com>
2023-01-11 22:35:27 +0600

    brand-section1

--- src/Pages/Home/Home/BrandSection/BrandSection.js
+++ src/Pages/Home/Home/BrandSection/BrandSection.js
@@ -2,8 +2,23 @@ import React from 'react';
 
 const BrandSection = () => {
     return (
-        <div>
-            <h1 className='text-4xl text-center text-white'>Brand Section</h1>
+        <div className='my-12 text-white '>
+            <h1 className='text-center text-4xl font-semibold' >Brand section</h1>
+            {/* bandCard */}
+            {/* <div className='grid lg:grid-cols-6 gap-1 justify-items-center mt-7'>
+                <div className="card  w-2/4 h-3/4 bg-base-100 shadow-xl">
+                    <div className='text-center'><figure><img src={brand8} alt="Shoes" className='rounded-2xl w-full ' /></figure></div>
+                </div>
+                <div className="card  w-2/4 h-3/4 bg-base-100 shadow-xl">
+                    <div className='text-center'><figure><img src={brand8} alt="Shoes" className='rounded-2xl w-full ' /></figure></div>
+                </div>
+                <div className="card  w-2/4 h-3/4 bg-base-100 shadow-xl">
+                    <div className='text-center'><figure><img src={brand8} alt="Shoes" className='rounded-2xl w-full ' /></figure></div>
+                </div>
+                <div className="card  w-2/4 h-3/4 bg-base-100 shadow-xl">
+                    <div className='text-center'><figure><img src={brand8} alt="Shoes" className='rounded-2xl w-full ' /></figure></div>
+                </div>
+            </div> */}
         </div>
     );
 };
