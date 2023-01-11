cc97e020237b30c35c27ee02cf5a93c14cecab81
hridoyhalder1 <hridoyhalder91@gmail.com>
2023-01-11 21:15:07 +0600

    brand image added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from './Banner/Banner';
+import BrandSection from './BrandSection/BrandSection';
 
 const Home = () => {
     return (
         <main>
             <Banner></Banner>
+            <BrandSection></BrandSection>
         </main>
     );
 };
