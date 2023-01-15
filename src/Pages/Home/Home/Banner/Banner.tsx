cc2c49ff2a6d158c642a17e7bf0f28dd88cd60b5
arifulislam38 <mdarifulislam0238@gmail.com>
2023-01-15 23:12:40 +0600

    Banner section corrected

--- src/Pages/Home/Home/Banner/Banner.tsx
+++ src/Pages/Home/Home/Banner/Banner.tsx
@@ -2,30 +2,32 @@ import React from "react";
 
 const Banner = () => {
   return (
-    <div className="font-poppins">
-      <div
-        className="hero min-h-screen"
-        style={{
-          backgroundImage: `url("https://i.ibb.co/sQ0PXpy/360-F-308445331-ZZinys-Rse5x-OZac-NTno-Qq-G24-TAy7ft-Z5.jpg")`
-        }}
-      >
-        <div className="hero-overlay bg-opacity-60 hover:bg-opacity-80"></div>
-        <div className="hero-content text-neutral-content">
-          <div className="lg:mr-96">
-            <h1 className="mb-5 lg:text-5xl text-3xl font-bold">
-              We are ready to give service <br />
-              To our customer <br />
-              Anytime anywhere
-            </h1>
-            <p className="mb-5">
-              We provide our best services to our customer. Our experts are well
-              knowend in our country.
-            </p>
-            <button className="btn btn-warning">Go to all services</button>
-          </div>
-        </div>
+    <section className="w-full h-screen relative overflow-hidden">
+
+      <div className="lg:w-[60%] w-full lg:h-full h-[70%] absolute lg:top-0 top-24 right-0 overflow-hidden bg-transparent">
+        <img
+          className="absolute h-full w-full"
+          src="assets/final1[84].png"
+          alt=""
+        />
       </div>
-    </div>
+
+      <div className="lg:w-[60%] w-full lg:h-full h-[70%] absolute top-24 lg:top-0 right-0 overflow-hidden bg-transparent hover:hidden">
+        <img
+          className="absolute  h-full w-full"
+          src="assets/slider-bg-005.png"
+          alt=""
+        />
+      </div>
+
+      <div className="w-full h-full absolute top-0 bg-black opacity-50 lg:hidden block"></div>
+
+      <div className="h-full lg:w-[30%] w-full absolute top-0 left-0 flex items-center justify-center">
+        <h1 className="text-5xl">
+          Maintenace and <br /> repair service
+        </h1>
+      </div>
+    </section>
   );
 };
 
