f4652f45b849afcc96077adefa71895ef32286c0
arifulislam38 <mdarifulislam0238@gmail.com>
2023-01-17 02:50:16 +0600

    popular service section polish

--- src/Pages/Home/Home/Banner/Banner.tsx
+++ src/Pages/Home/Home/Banner/Banner.tsx
@@ -1,5 +1,4 @@
- import React from "react";
-import './Banner.css';
+import "./Banner.css";
 
 const Banner = () => {
   return (
@@ -30,9 +29,14 @@ const Banner = () => {
         <div className="w-full h-full absolute top-0 bg-black opacity-50 lg:hidden block"></div>
 
         <div className="h-full lg:w-[40%] w-full absolute top-0 left-0 flex flex-col items-center justify-center">
-          <h1 className="text-7xl leading-[65px] font-semibold mb-6">
-          A genuine solution to vehicle problems
+          <h1 className="text-7xl leading-[70px] font-semibold mb-6 ">
+            A genuine solution to vehicle problems
           </h1>
+          <p className="text-xl">
+            Engine experts is one of the leading units speciliazing in providing
+            car service and give a new look to your cars. Also specialize in
+            modifying cars
+          </p>
           <button className="getbtn">Get Service</button>
         </div>
       </div>
