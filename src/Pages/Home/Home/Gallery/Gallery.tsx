0acb76702a2d9f0342da69980ceb0bdbdb72e9cb
nazrul-islam <nazrulislamrongon@gmail.com>
2023-01-17 22:23:15 +0600

    Images Changes

--- src/Pages/Home/Home/Gallery/Gallery.tsx
+++ src/Pages/Home/Home/Gallery/Gallery.tsx
@@ -6,22 +6,22 @@ const Gallery = () => {
         <h1 className="font-poppins text-center text-5xl font-bold my-14">Service Gallery</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         <div className=" bg-gray-600 rounded-lg">
-          <img className="rounded-lg p-1" src="/assets/gallery-1.png" alt="" />
+          <img className="rounded-lg p-1 justify-center items-center" src="/assets/gallery/gallery-1.png" alt="" />
         </div>
         <div className=" bg-gray-600 rounded-lg">
-          <img className="rounded-lg p-1" src="/assets/gallery-2.png" alt="" />
+          <img className="rounded-lg p-1" src="/assets/gallery/gallery-2.png" alt="" />
         </div>
         <div className=" bg-gray-600 rounded-lg">
-          <img className="rounded-lg p-1" src="/assets/gallery-3.png" alt="" />
+          <img className="rounded-lg p-1" src="/assets/gallery/gallery-3.png" alt="" />
         </div>
         <div className=" bg-gray-600 rounded-lg">
-          <img className="rounded-lg p-1" src="/assets/gallery-4.png" alt="" />
+          <img className="rounded-lg p-1" src="/assets/gallery/gallery-4.png" alt="" />
         </div>
         <div className=" bg-gray-600 rounded-lg">
-          <img className="rounded-lg p-1" src="/assets/gallery-5.png" alt="" />
+          <img className="rounded-lg p-1" src="/assets/gallery/gallery-5.png" alt="" />
         </div>
         <div className=" bg-gray-600 rounded-lg">
-          <img className="rounded-lg p-1" src="/assets/gallery-6.png" alt="" />
+          <img className="rounded-lg p-1" src="/assets/gallery/gallery-6.png" alt="" />
         </div>
       </div>
     </section>
