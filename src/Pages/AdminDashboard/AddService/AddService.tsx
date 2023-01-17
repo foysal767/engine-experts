6ffa86d06ec081bd60fa9e28525367756974ea54
abrarasif11 <faasif929@gmail.com>
2023-01-18 00:09:40 +0600

    edit-add-service

--- src/Pages/AdminDashboard/AddService/AddService.tsx
+++ src/Pages/AdminDashboard/AddService/AddService.tsx
@@ -3,16 +3,16 @@ import React from 'react';
 const AddService = () => {
     return (
         <div className='flex-1 border'>
-           <section className='mt-7'>
+          <section className='mt-7'>
            <input type="text" placeholder="Service Name" className=" input w-full max-w-xs" />
             <br />
             <textarea className="textarea textarea-bordered input w-full max-w-xs " placeholder="Service Description"></textarea>
             <br />
-            <input type="file" className="file-input w-full max-w-xs" />
-            <br />
             <input type="text" placeholder="Service Price" className=" input w-full max-w-xs" />
             <br />
-            <button className="btn btn-wide">Add Service</button>
+            <input type="file" className="file-input w-full max-w-xs" />
+            <br />
+            <button className="btn btn-wide mt-3 mb-3">Add Service</button>
            </section>
         </div>
     );
