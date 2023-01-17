2cedec5cae1eaf5e8ea3f5567ee264c55f21c017
SkPipul <skjabed252@gmail.com>
2023-01-17 22:14:57 +0600

    service detail added

--- src/Router/Router.tsx
+++ src/Router/Router.tsx
@@ -1,8 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main/Main";
+import AllServices from "../Pages/AllServices/AllServices";
 import ContactUs from "../Pages/Home/ContactUs/ContactUs";
 import Home from "../Pages/Home/Home/Home";
 import Login from "../Pages/Login/Login";
+import Services from "../Pages/Services/Services";
 import SignUp from "../Pages/SignUp/SignUp";
 
 const router = createBrowserRouter([
@@ -25,6 +27,14 @@ const router = createBrowserRouter([
             {
                 path: '/contactUs',
                 element: <ContactUs></ContactUs>
+            },
+            {
+                path: '/services',
+                element: <Services></Services>
+            },
+            {
+                path: '/allservices',
+                element: <AllServices></AllServices>
             }
         ]
     }
