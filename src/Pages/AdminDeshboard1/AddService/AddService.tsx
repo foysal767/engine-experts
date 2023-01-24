8b93fe475f9d023abd85904451d275750cd06b8a
arifulislam38 <mdarifulislam0238@gmail.com>
2023-01-25 02:49:20 +0600

    add service

---
+++ src/Pages/AdminDeshboard1/AddService/AddService.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddService = () => {
+    return (
+        <section>
+            this is the addservice section
+        </section>
+    );
+};
+
+export default AddService;
