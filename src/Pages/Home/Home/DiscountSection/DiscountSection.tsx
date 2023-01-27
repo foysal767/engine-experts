fa1609f1576a7d3e9b2ea9247afbe6e469817c84
SkPipul <skjabed252@gmail.com>
2023-01-27 11:24:14 +0600

    polishing discount section

--- src/Pages/Home/Home/DiscountSection/DiscountSection.tsx
+++ src/Pages/Home/Home/DiscountSection/DiscountSection.tsx
@@ -1,72 +1,3 @@
-// const DiscountSection = () => {
-//   return (
-//     <section className="w-full px-4 md:px-8 lg:px-12 lg:mb-32">
-//       <h2 className="font-poppins text-center text-5xl font-bold lg:mb-12">
-//         Discount for our customer
-//       </h2>
-//       <div
-//         data-aos="slide-up"
-//         data-aos-easing="linear"
-//         data-aos-duration="1000"
-//         className="w-full grid font-poppins lg:grid-cols-3 md:grid-cols-2  gap-3 place-items-center"
-//       >
-//         <div className="h-[250px] w-full relative">
-//           <img src="assets/car-washing.png" alt="" className="w-full h-full" />
-//           <div className="w-full h-full absolute top-0 bg-black opacity-40"></div>
-//           <div className="w-full absolute bottom-5 flex flex-col text-start pl-7">
-//             <h1 className="text-red-600 text-sm">Limited time offer</h1>
-//             <h2 className="text-white text-3xl font-semibold">
-//               Car Washing Service
-//             </h2>
-//           </div>
-//           <div className="text-3xl rounded-full h-[70px] w-[70px] grid place-items-center bg-red-600 absolute top-5 right-5 z-10">
-//             <h1 className="font-semibold m-0">
-//               50<span className="text-sm">%</span>
-//             </h1>
-//           </div>
-//           <div className="text-3xl rounded-full h-[70px] w-[70px] grid place-items-center bg-red-300 absolute top-6 right-4"></div>
-//         </div>
-
-//         <div className="h-[250px] w-full relative">
-//           <img src="assets/tire-change.png" alt="" className="w-full h-full" />
-//           <div className="w-full h-full absolute top-0 bg-black opacity-40"></div>
-//           <div className="w-full absolute bottom-5 flex flex-col text-start pl-7">
-//             <h1 className="text-red-600 text-sm">Limited time offer</h1>
-//             <h2 className="text-white text-3xl font-semibold">
-//               Performance upgrade
-//             </h2>
-//           </div>
-//           <div className="text-3xl rounded-full h-[70px] w-[70px] grid place-items-center bg-red-600 absolute top-5 right-5 z-10">
-//             <h1 className="font-semibold m-0">
-//               50<span className="text-sm">%</span>
-//             </h1>
-//           </div>
-//           <div className="text-3xl rounded-full h-[70px] w-[70px] grid place-items-center bg-red-300 absolute top-6 right-4"></div>
-//         </div>
-
-//         <div className="h-[250px] w-full relative">
-//           <img src="assets/engine repair.png" alt="" className="w-full h-full" />
-//           <div className="w-full h-full absolute top-0 bg-black opacity-40"></div>
-//           <div className="w-full absolute bottom-5 flex flex-col text-start pl-7">
-//             <h1 className="text-red-600 text-sm">Limited time offer</h1>
-//             <h2 className="text-white text-3xl font-semibold">
-//               Car Washing Service
-//             </h2>
-//           </div>
-//           <div className="text-3xl rounded-full h-[70px] w-[70px] grid place-items-center bg-red-600 absolute top-5 right-5 z-10">
-//             <h1 className="font-semibold m-0">
-//               50<span className="text-sm">%</span>
-//             </h1>
-//           </div>
-//           <div className="text-3xl rounded-full h-[70px] w-[70px] grid place-items-center bg-red-300 absolute top-6 right-4"></div>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default DiscountSection;
-
 import React from "react";
 import { AiFillStar } from "react-icons/ai";
 
@@ -79,7 +10,7 @@ const DiscountSection = () => {
           <img src="/assets/tire-right.png" alt="" className="tyre" />
         </div>
         <div className="absolute top-0 place-items-center w-full h-full mt-5">
-          <h2 className="font-bold text-center ">
+          <h2 className="font-extrabold text-center text-black text-xl">
             Hurry up! Discount up to 30%
           </h2>
           <div className="flex gap-x-5 justify-center mt-5">
