ad057129ab258805950e6bdf4c80117a6d4e1f55
arifulislam38 <mdarifulislam0238@gmail.com>
2023-01-29 20:41:55 +0600

    responsive

--- src/Pages/Home/Home/Home.tsx
+++ src/Pages/Home/Home/Home.tsx
@@ -1,38 +1,39 @@
-import React from 'react';
-import Services from '../../Services/Services';
-import WorkingEnvironment from '../WorkingEnvironment/WorkingEnvironment';
-import Banner from './Banner/Banner';
-import Brand from './BrandSection/Brand';
+import React from "react";
+import Services from "../../Services/Services";
+import WorkingEnvironment from "../WorkingEnvironment/WorkingEnvironment";
+import Banner from "./Banner/Banner";
+import Brand from "./BrandSection/Brand";
+// import Brand from "./BrandSection/Brand";
 // import BrandSection from './BrandSection/BrandSection';
-import CustomerReviews from './CustomerReviews/CustomerReviews';
-import DiscountSection from './DiscountSection/DiscountSection';
-import EngineExperties from './EnginrExperties/EngineExperties';
-import GetInTouch from './GetInTouch/GetInTouch';
+import CustomerReviews from "./CustomerReviews/CustomerReviews";
+import DiscountSection from "./DiscountSection/DiscountSection";
+import EngineExperties from "./EnginrExperties/EngineExperties";
+import GetInTouch from "./GetInTouch/GetInTouch";
 // import Gallery from './Gallery/Gallery';
 // import GetService from './GetService/GetService';
 // import OurTeam from './OurTeam/OurTeam';
-import PopularService from './PopularService/PopularService';
-import Video from './Video/Video';
+import PopularService from "./PopularService/PopularService";
+import Video from "./Video/Video";
 
 const Home = () => {
-    return (
-        <main>
-            <Banner></Banner>
-            <DiscountSection></DiscountSection>
-            <PopularService></PopularService>
-            <Services></Services>
-            <Video></Video>
-            <WorkingEnvironment></WorkingEnvironment>
-            {/* <Gallery></Gallery> /}
+  return (
+    <main>
+      <Banner></Banner>
+      <DiscountSection></DiscountSection>
+      <PopularService></PopularService>
+      <Services></Services>
+      <Video></Video>
+      <WorkingEnvironment></WorkingEnvironment>
+      {/* <Gallery></Gallery> /}
             <OurTeam></OurTeam>
             {/ <GetService></GetService> */}
-            <EngineExperties></EngineExperties>
-            <GetInTouch></GetInTouch>
-            <CustomerReviews></CustomerReviews>
-            <Brand></Brand>
-            {/* <BrandSection></BrandSection> */}
-        </main>
-    );
+      <EngineExperties></EngineExperties>
+      <GetInTouch></GetInTouch>
+      <CustomerReviews></CustomerReviews>
+      <Brand></Brand>
+      {/* <BrandSection></BrandSection> */}
+    </main>
+  );
 };
 
 export default Home;
