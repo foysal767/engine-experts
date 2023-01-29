bc64d727e7b813c4de205cd1a5410bcfa26b92f3
Km-Nazrul-Islam <nazrulislamrongon@gmail.com>
2023-01-29 22:50:16 +0600

    Get In Touch

--- src/Pages/Home/Home/EnginrExperties/EngineExperties.tsx
+++ src/Pages/Home/Home/EnginrExperties/EngineExperties.tsx
@@ -2,7 +2,7 @@ import "./EngineExperties.css"
 
 const EngineExperties = () => {
   return (
-    <section className="h-[174vh] lg:h-[85vh] blog mb-6 lg:mb-28 px-4 md:px-8 lg:px-12 bg-[#19191B] bg-[url('/public/assets/image-gallery/carbon_BG-20.png')] w-full grid place-items-center">
+    <section className="h-[120vh] md:h-[65vh] lg:h-[85vh] blog mb-6 lg:mb-28 px-4 md:px-8 lg:px-12 bg-[#19191B] bg-[url('/public/assets/image-gallery/carbon_BG-20.png')] w-full grid place-items-center">
       <div className="w-full lg:w-[90%] mx-auto h-full relative ">
         <h1 className="cssStyle text-4xl font-bold text-left pt-6">
           Our Experties
@@ -114,96 +114,3 @@ const EngineExperties = () => {
 }
 
 export default EngineExperties
-
-/**
- * <section className="px-10 my-20 bg-red-700">
-      <div className="mx-36">
-        <h2 className="text-4xl text-left font-bold">Our Engine Experties</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-20 justify-evenly">
-          <div className="engineWrapper flex">
-            <div className="leftEngine flex flex-col">
-              <img
-                className="w-72 h-84 absolute"
-                src="/assets/service-1.1.jpg"
-                alt=""
-              />
-              <div className="bg-white text-black relative top-44 ml-3 hover:bg-red-300">
-                <div className="my-4">
-                  <div className="flex gap-20">
-                    <div className="byAdmin flex gap-2 items-center">
-                      <FaRegUser className="text-red-500"></FaRegUser>
-                      <span>by Admin</span>
-                    </div>
-                    <div className="service flex gap-2 items-center">
-                      <FaServicestack className="text-red-500"></FaServicestack>
-                      <span>Service</span>
-                    </div>
-                  </div>
-                </div>
-                <div className="text-xl text-gray-800 font-bold mb-4">
-                  Common Engine Oil <br />
-                  Problems and Solutions
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="engineWrapper flex">
-            <div className="leftEngine flex flex-col">
-              <img
-                className="w-72 h-84 absolute"
-                src="/assets/image-gallery/Performance-upgrade.jpg"
-                alt=""
-              />
-              <div className="bg-white text-black relative top-44 ml-3 hover:bg-red-300">
-                <div className="my-4">
-                  <div className="flex gap-20">
-                    <div className="byAdmin flex gap-2 items-center">
-                      <FaRegUser className="text-red-500"></FaRegUser>
-                      <span>by Admin</span>
-                    </div>
-                    <div className="service flex gap-2 items-center">
-                      <FaServicestack className="text-red-500"></FaServicestack>
-                      <span>Service</span>
-                    </div>
-                  </div>
-                </div>
-                <div className="text-xl text-gray-800 font-bold mb-4">
-                  Common Engine Oil <br />
-                  Problems and Solutions
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="engineWrapper flex">
-            <div className="leftEngine flex flex-col">
-              <img
-                className="w-72 h-84 absolute"
-                src="/assets/contact/contact3.jpg"
-                alt=""
-              />
-              <div className="bg-white text-black relative top-44 ml-3 hover:bg-red-300">
-                <div className="my-4">
-                  <div className="flex gap-20">
-                    <div className="byAdmin flex gap-2 items-center">
-                      <FaRegUser className="text-red-500"></FaRegUser>
-                      <span>by Admin</span>
-                    </div>
-                    <div className="service flex gap-2 items-center">
-                      <FaServicestack className="text-red-500"></FaServicestack>
-                      <span>Service</span>
-                    </div>
-                  </div>
-                </div>
-                <div className="text-xl text-gray-800 font-bold mb-4">
-                  Common Engine Oil <br />
-                  Problems and Solutions
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
- */
