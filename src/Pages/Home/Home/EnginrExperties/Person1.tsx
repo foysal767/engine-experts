d98848b55871693d90e89016285c02c4eabfc8d3
Km-Nazrul-Islam <nazrulislamrongon@gmail.com>
2023-02-07 17:19:01 +0600

    Expertise Details

--- src/Pages/Home/Home/EnginrExperties/Person1.tsx
+++ src/Pages/Home/Home/EnginrExperties/Person1.tsx
@@ -1,11 +1,361 @@
 const Person1 = () => {
   return (
     <section>
-      <div>
-        <img src="assets/image-gellary/Air-Conditioning-&-Evac.jpg" alt="" />
-        <h2 className="text-4xl text-center text-red-700">
-          Work Experiance Of Ariful Islam
-        </h2>
+      <div className="my-20 flex gap-4 px-10">
+        <div className="sideBar text-black border-2 basis-3/12">
+          <img className="rounded-lg" src="assets/team/arif.jpg" alt="" />
+          <div className="flex justify-around text-2xl text-gray-700 font-bold my-2">
+            <h1>Name:</h1>
+            <span>Ariful Islam</span>
+          </div>
+          <hr className="border border-red-500 mb-2" />
+          <p className="text-xl text-gray-600">
+            Hi! I am Ariful Islam. I am a motor machanicqe. My job title is
+            Air-Conditioning-&-Evac Spacialist. To my company my position is a
+            senior excutive of engine expertise.
+          </p>
+          <div className="contactInfo mt-8">
+            <span className="text-3xl text-gray-600 font-bold">
+              Education Info
+            </span>
+            <hr className="border border-red-500 mb-2" />
+            <div className=" flex justify-around">
+              <h1>Email:</h1>
+              <span>arifulislam24@gmail.com</span>
+            </div>
+          </div>
+        </div>
+        <div className="rightBar text-black basis-9/12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          <div className="card shadow-xl">
+            <figure className="px-10 pt-10">
+              <img
+                src="assets/image-gallery/Air-Conditioning-&-Evac.jpg"
+                alt="Shoes"
+                className="rounded-xl"
+              />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">Air-Conditioning-&-Evac</h2>
+              <p>
+                Recovery is the process of taking the refrigerant out, not much
+                or any oil is going to come with it, until the system pressure
+                is 0. Vacuuming or evacuation is removing all gasses until the
+                system
+              </p>
+              <div className="card-actions">
+                <button className="btn btn-primary">Read More</button>
+              </div>
+            </div>
+          </div>
+
+          <div className="card shadow-xl">
+            <figure className="px-10 pt-10">
+              <img
+                src="assets/image-gallery/Air-Conditioning-&-Evac.jpg"
+                alt="Shoes"
+                className="rounded-xl"
+              />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">Air-Conditioning-&-Evac</h2>
+              <p>
+                Recovery is the process of taking the refrigerant out, not much
+                or any oil is going to come with it, until the system pressure
+                is 0. Vacuuming or evacuation is removing all gasses until the
+                system
+              </p>
+              <div className="card-actions">
+                <button className="btn btn-primary">Read More</button>
+              </div>
+            </div>
+          </div>
+
+          <div className="card shadow-xl">
+            <figure className="px-10 pt-10">
+              <img
+                src="assets/image-gallery/Air-Conditioning-&-Evac.jpg"
+                alt="Shoes"
+                className="rounded-xl"
+              />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">Air-Conditioning-&-Evac</h2>
+              <p>
+                Recovery is the process of taking the refrigerant out, not much
+                or any oil is going to come with it, until the system pressure
+                is 0. Vacuuming or evacuation is removing all gasses until the
+                system
+              </p>
+              <div className="card-actions">
+                <button className="btn btn-primary">Read More</button>
+              </div>
+            </div>
+          </div>
+
+          <div className="card shadow-xl">
+            <figure className="px-10 pt-10">
+              <img
+                src="assets/image-gallery/Air-Conditioning-&-Evac.jpg"
+                alt="Shoes"
+                className="rounded-xl"
+              />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">Air-Conditioning-&-Evac</h2>
+              <p>
+                Recovery is the process of taking the refrigerant out, not much
+                or any oil is going to come with it, until the system pressure
+                is 0. Vacuuming or evacuation is removing all gasses until the
+                system
+              </p>
+              <div className="card-actions">
+                <button className="btn btn-primary">Read More</button>
+              </div>
+            </div>
+          </div>
+
+          <div className="card shadow-xl">
+            <figure className="px-10 pt-10">
+              <img
+                src="assets/image-gallery/Air-Conditioning-&-Evac.jpg"
+                alt="Shoes"
+                className="rounded-xl"
+              />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">Air-Conditioning-&-Evac</h2>
+              <p>
+                Recovery is the process of taking the refrigerant out, not much
+                or any oil is going to come with it, until the system pressure
+                is 0. Vacuuming or evacuation is removing all gasses until the
+                system
+              </p>
+              <div className="card-actions">
+                <button className="btn btn-primary">Read More</button>
+              </div>
+            </div>
+          </div>
+
+          <div className="card shadow-xl">
+            <figure className="px-10 pt-10">
+              <img
+                src="assets/image-gallery/Air-Conditioning-&-Evac.jpg"
+                alt="Shoes"
+                className="rounded-xl"
+              />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">Air-Conditioning-&-Evac</h2>
+              <p>
+                Recovery is the process of taking the refrigerant out, not much
+                or any oil is going to come with it, until the system pressure
+                is 0. Vacuuming or evacuation is removing all gasses until the
+                system
+              </p>
+              <div className="card-actions">
+                <button className="btn btn-primary">Read More</button>
+              </div>
+            </div>
+          </div>
+
+          <div className="card shadow-xl">
+            <figure className="px-10 pt-10">
+              <img
+                src="assets/image-gallery/Air-Conditioning-&-Evac.jpg"
+                alt="Shoes"
+                className="rounded-xl"
+              />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">Air-Conditioning-&-Evac</h2>
+              <p>
+                Recovery is the process of taking the refrigerant out, not much
+                or any oil is going to come with it, until the system pressure
+                is 0. Vacuuming or evacuation is removing all gasses until the
+                system
+              </p>
+              <div className="card-actions">
+                <button className="btn btn-primary">Read More</button>
+              </div>
+            </div>
+          </div>
+
+          <div className="card shadow-xl">
+            <figure className="px-10 pt-10">
+              <img
+                src="assets/image-gallery/Air-Conditioning-&-Evac.jpg"
+                alt="Shoes"
+                className="rounded-xl"
+              />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">Air-Conditioning-&-Evac</h2>
+              <p>
+                Recovery is the process of taking the refrigerant out, not much
+                or any oil is going to come with it, until the system pressure
+                is 0. Vacuuming or evacuation is removing all gasses until the
+                system
+              </p>
+              <div className="card-actions">
+                <button className="btn btn-primary">Read More</button>
+              </div>
+            </div>
+          </div>
+
+          <div className="card shadow-xl">
+            <figure className="px-10 pt-10">
+              <img
+                src="assets/image-gallery/Air-Conditioning-&-Evac.jpg"
+                alt="Shoes"
+                className="rounded-xl"
+              />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">Air-Conditioning-&-Evac</h2>
+              <p>
+                Recovery is the process of taking the refrigerant out, not much
+                or any oil is going to come with it, until the system pressure
+                is 0. Vacuuming or evacuation is removing all gasses until the
+                system
+              </p>
+              <div className="card-actions">
+                <button className="btn btn-primary">Read More</button>
+              </div>
+            </div>
+          </div>
+
+          <div className="card shadow-xl">
+            <figure className="px-10 pt-10">
+              <img
+                src="assets/image-gallery/Air-Conditioning-&-Evac.jpg"
+                alt="Shoes"
+                className="rounded-xl"
+              />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">Air-Conditioning-&-Evac</h2>
+              <p>
+                Recovery is the process of taking the refrigerant out, not much
+                or any oil is going to come with it, until the system pressure
+                is 0. Vacuuming or evacuation is removing all gasses until the
+                system
+              </p>
+              <div className="card-actions">
+                <button className="btn btn-primary">Read More</button>
+              </div>
+            </div>
+          </div>
+
+          <div className="card shadow-xl">
+            <figure className="px-10 pt-10">
+              <img
+                src="assets/image-gallery/Air-Conditioning-&-Evac.jpg"
+                alt="Shoes"
+                className="rounded-xl"
+              />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">Air-Conditioning-&-Evac</h2>
+              <p>
+                Recovery is the process of taking the refrigerant out, not much
+                or any oil is going to come with it, until the system pressure
+                is 0. Vacuuming or evacuation is removing all gasses until the
+                system
+              </p>
+              <div className="card-actions">
+                <button className="btn btn-primary">Read More</button>
+              </div>
+            </div>
+          </div>
+
+          <div className="card shadow-xl">
+            <figure className="px-10 pt-10">
+              <img
+                src="assets/image-gallery/Air-Conditioning-&-Evac.jpg"
+                alt="Shoes"
+                className="rounded-xl"
+              />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">Air-Conditioning-&-Evac</h2>
+              <p>
+                Recovery is the process of taking the refrigerant out, not much
+                or any oil is going to come with it, until the system pressure
+                is 0. Vacuuming or evacuation is removing all gasses until the
+                system
+              </p>
+              <div className="card-actions">
+                <button className="btn btn-primary">Read More</button>
+              </div>
+            </div>
+          </div>
+
+          <div className="card shadow-xl">
+            <figure className="px-10 pt-10">
+              <img
+                src="assets/image-gallery/Air-Conditioning-&-Evac.jpg"
+                alt="Shoes"
+                className="rounded-xl"
+              />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">Air-Conditioning-&-Evac</h2>
+              <p>
+                Recovery is the process of taking the refrigerant out, not much
+                or any oil is going to come with it, until the system pressure
+                is 0. Vacuuming or evacuation is removing all gasses until the
+                system
+              </p>
+              <div className="card-actions">
+                <button className="btn btn-primary">Read More</button>
+              </div>
+            </div>
+          </div>
+
+          <div className="card shadow-xl">
+            <figure className="px-10 pt-10">
+              <img
+                src="assets/image-gallery/Air-Conditioning-&-Evac.jpg"
+                alt="Shoes"
+                className="rounded-xl"
+              />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">Air-Conditioning-&-Evac</h2>
+              <p>
+                Recovery is the process of taking the refrigerant out, not much
+                or any oil is going to come with it, until the system pressure
+                is 0. Vacuuming or evacuation is removing all gasses until the
+                system
+              </p>
+              <div className="card-actions">
+                <button className="btn btn-primary">Read More</button>
+              </div>
+            </div>
+          </div>
+
+          <div className="card shadow-xl">
+            <figure className="px-10 pt-10">
+              <img
+                src="assets/image-gallery/Air-Conditioning-&-Evac.jpg"
+                alt="Shoes"
+                className="rounded-xl"
+              />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">Air-Conditioning-&-Evac</h2>
+              <p>
+                Recovery is the process of taking the refrigerant out, not much
+                or any oil is going to come with it, until the system pressure
+                is 0. Vacuuming or evacuation is removing all gasses until the
+                system
+              </p>
+              <div className="card-actions">
+                <button className="btn btn-primary">Read More</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   )
