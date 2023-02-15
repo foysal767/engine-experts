6c7f610eab58ce0fcb71cebebbcdde79ff355161
Km-Nazrul-Islam <nazrulislamrongon@gmail.com>
2023-02-15 12:28:43 +0600

    Project Email Added in Contact Form

--- src/Pages/Home/ContactForm/ContactForm.tsx
+++ src/Pages/Home/ContactForm/ContactForm.tsx
@@ -33,8 +33,10 @@ const ContactForm = () => {
       <div className="contactForm grid lg:grid-cols-3 md:grid-cols-2 gap-6 justify-center">
         <div className="leftContactForm lg:col-span-2 p-4 rounded-lg lg:px-20 py-10 bg-[#19191B] bg-[url('/public/assets/image-gallery/carbon_BG-20.png')]">
           <div className="headText text-left">
-            <h2 className="text-4xl font-semibold">Need Help ? Send Message</h2>
-            <p className="text-xl mb-4">
+            <h2 className="text-4xl font-semibold font-poppins">
+              Need Help ? Send Message
+            </h2>
+            <p className="text-xl mb-4 font-poppins">
               We provide all engine services related help!
             </p>
           </div>
@@ -43,12 +45,12 @@ const ContactForm = () => {
               <div className="nameEmail grid lg:grid-cols-2 md:grid-cols-1 gap-4">
                 <div className="inputName">
                   <div className="font-bold text-left">
-                    <label className="" htmlFor="name">
+                    <label className="font-poppins" htmlFor="name">
                       Name
                     </label>
                   </div>
                   <input
-                    className="bg-gray-100 input input-bordered text-black w-full"
+                    className="bg-gray-100 input input-bordered text-black w-full font-poppins"
                     type="text"
                     name="name"
                     id="name"
@@ -57,12 +59,12 @@ const ContactForm = () => {
                 </div>
                 <div className="inputEmail">
                   <div className="font-bold text-left">
-                    <label className="" htmlFor="email">
+                    <label className="font-poppins" htmlFor="email">
                       Email
                     </label>
                   </div>
                   <input
-                    className="bg-gray-100 input input-bordered text-black w-full"
+                    className="bg-gray-100 input input-bordered text-black w-full font-poppins"
                     type="email"
                     name="email"
                     id="email"
@@ -73,12 +75,12 @@ const ContactForm = () => {
               <div className="phoneService grid lg:grid-cols-2 md:grid-cols-1 gap-4 my-4">
                 <div className="inputPhone">
                   <div className="font-bold text-left">
-                    <label className="" htmlFor="phone">
+                    <label className="font-poppins" htmlFor="phone">
                       Phone
                     </label>
                   </div>
                   <input
-                    className="bg-gray-100 input input-bordered text-black w-full"
+                    className="bg-gray-100 input input-bordered text-black w-full font-poppins"
                     type="text"
                     name="phone"
                     id="phone"
@@ -87,12 +89,12 @@ const ContactForm = () => {
                 </div>
                 <div className="inputService">
                   <div className="font-bold text-left">
-                    <label className="" htmlFor="service">
+                    <label className="font-poppins" htmlFor="service">
                       Service
                     </label>
                   </div>
                   <input
-                    className="bg-gray-100 input input-bordered text-black w-full"
+                    className="bg-gray-100 input input-bordered text-black w-full font-poppins"
                     type="text"
                     name="service"
                     id="service"
@@ -102,12 +104,12 @@ const ContactForm = () => {
               </div>
               <div>
                 <div className="font-bold text-left">
-                  <label className="" htmlFor="message">
+                  <label className="font-poppins" htmlFor="message">
                     Message
                   </label>
                 </div>
                 <textarea
-                  className="textarea textarea-bordered text-black bg-gray-100 mb-4 w-full"
+                  className="textarea textarea-bordered text-black bg-gray-100 mb-4 w-full font-poppins"
                   placeholder="Your Message"
                   name="message"
                   id="massage"
@@ -115,24 +117,32 @@ const ContactForm = () => {
               </div>
 
               <div>
-                <button className="contactBtn font-bold">Submit</button>
+                <button className="contactBtn font-bold font-poppins">
+                  Submit
+                </button>
               </div>
             </form>
           </div>
         </div>
         <div className="rightContactDetails p-4 rounded-lg py-10 px-8 bg-[#19191B] bg-[url('/public/assets/image-gallery/carbon_BG-20.png')]">
           <div className="contactDetails text-left mb-8">
-            <h2 className="text-4xl font-semibold  mb-4">Contact Details</h2>
-            <p className="">
-              <span className="font-bold ">Office Address -1: </span> Engine
-              Experts Agency <br />
-              (Chottogram Baskhali) Co. Ltd Bridge 8. Room <br /> 9201
+            <h2 className="text-4xl font-semibold  mb-4 font-poppins">
+              Contact Details
+            </h2>
+            <p className="font-poppins">
+              <span className="font-bold font-poppins">
+                Office Address -1:{" "}
+              </span>{" "}
+              Engine Experts Agency <br />
+              (Chottogram Baskhali) Co. Ltd Bridge 8. Room Chottgram Bangladesh
+              - 9201
             </p>
             <br />
-            <p className="">
-              <span className="font-bold ">Office Address-2: </span> Engine
-              Expertice Agency <br />
-              (Kamranggir Chor Vuter Goli) Co. Ltd Sheik Jamal Road- <br /> 9876
+            <p className="font-poppins">
+              <span className="font-bold font-poppins">Office Address-2: </span>{" "}
+              Engine Expertice Agency <br />
+              (Kamranggir Chor Vuter Goli) Co. Ltd Sheik Jamal Road- Dhaka,
+              Bangladesh - 9876
             </p>
           </div>
           <div className="emergencyCall flex items-center space-x-4 mb-4">
@@ -140,8 +150,10 @@ const ContactForm = () => {
               <FaPhoneVolume />
             </div>
             <div>
-              <h2 className="text-sm  text-left">Emargency Call</h2>
-              <span className="font-bold ">+88 017 22 262041</span>
+              <h2 className="text-sm  text-left font-poppins">
+                Emargency Call
+              </h2>
+              <span className="font-bold font-poppins">+88 017 22 262041</span>
             </div>
           </div>
           <div className="generalEmail flex items-center space-x-4">
@@ -149,8 +161,12 @@ const ContactForm = () => {
               <FaRegEnvelope />
             </div>
             <div>
-              <h2 className="text-sm  text-left">General Communication</h2>
-              <span className="font-bold ">nazrulislamrongon@gmail.com</span>
+              <h2 className="text-sm  text-left font-poppins">
+                General Communication
+              </h2>
+              <span className="font-bold font-poppins">
+                engineexpertise24@gmail.com
+              </span>
             </div>
           </div>
         </div>
