e1473156bf7545bc2c1e35361391900885edd680
Km-Nazrul-Islam <nazrulislamrongon@gmail.com>
2023-02-18 23:03:43 +0600

    Latest Blog Added

--- src/Pages/Blog/Blog.tsx
+++ src/Pages/Blog/Blog.tsx
@@ -1,10 +1,12 @@
 import BlogBanner from "./BlogBanner/BlogBanner"
 import BlogService from "./BlogService/BlogService"
+import LatestBlog from "./LatestBlog/LatestBlog"
 const Blog = () => {
   return (
     <main>
       <BlogBanner></BlogBanner>
       <BlogService></BlogService>
+      <LatestBlog></LatestBlog>
     </main>
   )
 }
