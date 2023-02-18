31987267f74abfa0650bb071df0e294de184352c
Km-Nazrul-Islam <nazrulislamrongon@gmail.com>
2023-02-18 17:44:00 +0600

    Blog Banner Image Added

--- src/Pages/Blog/BlogBanner/BlogBanner.tsx
+++ src/Pages/Blog/BlogBanner/BlogBanner.tsx
@@ -4,38 +4,30 @@ import "./BlogBanner.css"
 const BlogBanner = () => {
   return (
     <section className="banner md:h-[40vh]  w-[100%] lg:px-14 md:px-8 px-4 h-[70vh] lg:h-screen relative overflow-hidden text-start bg-[#19191B] bg-[url('/public/assets/image-gallery/carbon_BG-20.png')] mb-12 lg:mb-28">
-      <div className="w-full h-full flex">
+      <div className="w-full h-full flex flex-col md:flex-row lg:flex-row justify-around">
         <div className="h-full w-full lg:w-[40%] flex flex-col items-center justify-center md:text-center lg:text-left">
           <h1 className="text-2xl lg:text-[3vw] lg:leading-[50px] font-semibold mb-6 text-[#FFFFFF] md:text-4xl ">
-            A genuine solution to vehicle problems
+            Latest Blog Of Engine Expertise
           </h1>
-          <p className="md:text-xl text-lg">
-            Engine experts is one of the leading units speciliazing in providing
-            car service and give a new look to your cars. Also specialize in
-            modifying cars.
+          <p className="md:text-2xl text-lg">
+            Blogs and Forums Don't forget to check out our forums for Lowrider
+            Magazine and Lowrider Arte.
           </p>
-          <div className="w-full flex items-start md:items-center md:ml-[550px] lg:ml-0">
-            <Link to="/servicesAll">
-              <button className="bannerBtn mt-6">Get Service</button>
-            </Link>
+          <div className="flex flex-col md:flex-row lg:flex-row gap-4">
+            <div className="w-full flex items-start md:items-center md:ml-[550px] lg:ml-0">
+              <Link to="/home">
+                <button className="bannerBtn mt-6">Home</button>
+              </Link>
+            </div>
+            <div className="w-full flex items-start md:items-center md:ml-[550px] lg:ml-0">
+              <Link to="/home">
+                <button className="bannerBtn mt-6">Blog</button>
+              </Link>
+            </div>
           </div>
         </div>
-        <div className="w-full hidden lg:w-[60%] lg:grid place-items-center h-full bannerCar  relative">
-          <img
-            className="h-[94%] w-full absolute top-0 bannerImg"
-            src="assets/slider-bg-005.png"
-            alt=""
-          />
-          <img
-            className="h-[94%] w-full absolute top-0 bannerLightImg"
-            src="assets/final1[84].png"
-            alt=""
-          />
-          <img
-            className="absolute top-[48.6%] right-[46.2%] rotation  z-10 hidden lg:block"
-            src="assets/circle4[90].png"
-            alt=""
-          />
+        <div className="w-full lg:w-[40%] lg:grid place-items-center justify-end h-full bannerCar">
+          <img className="" src="assets/blog/bannarBlog6.png" alt="" />
         </div>
       </div>
     </section>
