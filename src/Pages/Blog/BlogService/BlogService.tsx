21654f1ab0dbf09fe9804d8bf0b0a1ccdfe5866d
Km-Nazrul-Islam <nazrulislamrongon@gmail.com>
2023-02-19 18:08:26 +0600

    Transmission Blog Added

--- src/Pages/Blog/BlogService/BlogService.tsx
+++ src/Pages/Blog/BlogService/BlogService.tsx
@@ -9,17 +9,35 @@ const BlogService = () => {
           </h1>
 
           <div className="mb-10 border border-gray-500 p-4 rounded-lg cursor-pointer">
-            <h1 className="text-gray-600">
-              How Automotive Air Conditioning Works?
-            </h1>
+            <h1 className="text-gray-600">How Evac Air Conditioning Works?</h1>
             <img
               src="assets/engine-blog/air-engine/airEngine-5.jpg"
               className="animated w-full my-4 rounded-lg"
               alt=""
             />
             <p className="text-gray-600 mb-4">
-              The air-conditioning system in a car works by manipulating
-              refrigerant between a liquid and a gaseous state....
+              <div className="collapse">
+                <input type="checkbox" />
+                <div className="collapse-title text-xl font-medium">
+                  Details Evac
+                </div>
+                <div className="collapse-content">
+                  <p>
+                    These formidable engines, our six Best Engines evaluators
+                    believe, are — or, for repeat winners, remain — benchmark
+                    executions of engine design, innovation, manufacturing. They
+                    excel in a business that holds their "contribution" to the
+                    overall product in the highest regard and stand proud of the
+                    conventional For 2001’s installment, nothing about the Best
+                    Engines contest has changed. Each engine nominated by our
+                    six editors must stand up to every other nominated engine in
+                    a battle unsullied by the marketing-happy "category
+                    competition" that so devalues other industry awards. We
+                    don’t "sell" the Best Engines to anybody. These engines have
+                    to sell themselves to us.
+                  </p>
+                </div>
+              </div>
             </p>
           </div>
 
@@ -63,15 +81,53 @@ const BlogService = () => {
           </div>
         </div>
         <div className="middleBar basis-9/12">
-          <div className="mb-10 flex flex-col-reverse md:flex-row lg:flex-row gap-4 justify-center items-center cursor-pointer hover:bg-black hover:bg-[url('/public/assets/image-gallery/carbon_BG-20.png')] text-gray-600 hover:text-white p-4">
+          <div className="mb-10 flex flex-col-reverse md:flex-row lg:flex-row gap-4 justify-center items-center cursor-pointer hover:bg-black hover:bg-[url('/public/assets/image-gallery/carbon_BG-20.png')] text-gray-600 hover:text-white p-4 rounded-lg">
             <div className="">
-              <h1 className="text-2xl text-center font-bold ">
+              <h1 className="text-2xl text-left font-bold ">
                 Blog Of How Automotive Air Conditioning Works ?
               </h1>
               <p className="text-xl text-justify mt-4">
-                When asked how car air conditioning systems work, most people
-                would respond, “I don’t care how they work, I just want them to
-                work!” While that may be a common response
+                <div className="collapse">
+                  <input type="checkbox" />
+                  <div className="collapse-title">
+                    When asked how car air conditioning systems work, most
+                    people would respond, “I don’t care how they work, I just
+                    want them to work!” While that may be a common response{" "}
+                    <br />
+                    <span className="text-red-500">Read More</span>
+                  </div>
+                  <div className="collapse-content">
+                    <p>
+                      <span className="text-2xl font-bold">
+                        How Does AC Work in a Car?
+                      </span>
+                      <br />
+                      The air-conditioning system in a car works by manipulating
+                      refrigerant between a liquid and a gaseous state. As the
+                      refrigerant changes states, it absorbs heat and humidity
+                      from the vehicle and allows the system to give off cool,
+                      dry air.
+                      <br />
+                      <br />
+                      To change the refrigerant between a liquid and a gaseous
+                      state, the air-conditioning system works to control
+                      pressure and temperature.
+                      <br />
+                      <br />
+                      <span className="text-2xl font-bold">
+                        AC System Refrigerants
+                      </span>
+                      <br />
+                      <br />
+                      In the past, automotive air condition systems used R-12 as
+                      the refrigerant. R-12 (aka Freon) is a very effective
+                      CFC-based (chlorofluorocarbon) refrigerant that is not
+                      flammable and not poisonous to humans. During the late
+                      1980s, scientists discovered that widespread usage of R-12
+                      was damaging the earth’s ozone layer
+                    </p>
+                  </div>
+                </div>
               </p>
             </div>
             <img
@@ -82,16 +138,53 @@ const BlogService = () => {
           </div>
           <hr className="text-2xl border-red-400 border-b-2 mb-12" />
 
-          <div className="mb-10 flex flex-col-reverse md:flex-row lg:flex-row gap-4 justify-center items-center cursor-pointer hover:bg-black hover:bg-[url('/public/assets/image-gallery/carbon_BG-20.png')] text-gray-600 hover:text-white p-4">
-            <div>
-              <h1 className="text-2xl text-center font-bold ">
-                Blog Expansion Valve System
+          <div className="mb-10 flex flex-col-reverse md:flex-row lg:flex-row gap-4 justify-center items-center cursor-pointer hover:bg-black hover:bg-[url('/public/assets/image-gallery/carbon_BG-20.png')] text-gray-600 hover:text-white p-4 rounded-lg">
+            <div className="">
+              <h1 className="text-2xl text-left font-bold ">
+                Blog Of Diagonstic teasting ?
               </h1>
               <p className="text-xl text-justify mt-4">
-                Expansion valves are devices used to control the refrigerant
-                flow in a refrigeration system. They help to facilitate the
-                change of higher pressure of liquid refrigerant in the
-                condensing unit
+                <div className="collapse">
+                  <input type="checkbox" />
+                  <div className="collapse-title">
+                    Diagnostic tests are variety of procedures done by
+                    physicians to screen for, detect and monitor diseases and
+                    conditions.
+                    <br />
+                    <span className="text-red-500">Read More</span>
+                  </div>
+                  <div className="collapse-content">
+                    <p>
+                      <span className="text-2xl font-bold">
+                        How Does Transmission?
+                      </span>
+                      <br />
+                      Diagnostic tests are variety of procedures done by
+                      physicians to screen for, detect and monitor diseases and
+                      conditions.
+                      <br />
+                      <br />
+                      It is used to gather clinical information necessary for
+                      making a diagnosis. The scope of diagnostic test ranges
+                      from signs and symptoms elicited during imaging tests,
+                      clinical examination, to pathologic,
+                      <br />
+                      <br />
+                      <span className="text-2xl font-bold">
+                        Transmissin working
+                      </span>
+                      <br />
+                      <br />
+                      biochemical, and psychological tests. Getting the right
+                      diagnosis is a crucial aspect of health care or clinical
+                      practice as it provides an explanation of a patient's
+                      health problem and informs whether a patient has or does
+                      not have a condition. Adequate degree of reliability and
+                      validity are the most basic parameters that need to be
+                      established about any clinical test.
+                    </p>
+                  </div>
+                </div>
               </p>
             </div>
             <img
@@ -102,16 +195,57 @@ const BlogService = () => {
           </div>
           <hr className="text-2xl border-red-400 border-b-2 mb-12" />
 
-          <div className="mb-10 flex flex-col-reverse md:flex-row lg:flex-row gap-4 justify-center items-center cursor-pointer hover:bg-black hover:bg-[url('/public/assets/image-gallery/carbon_BG-20.png')] text-gray-600 hover:text-white p-4">
-            <div>
-              <h1 className="text-2xl text-center font-bold ">
-                Blog Orifice Tube system
+          <div className="mb-10 flex flex-col-reverse md:flex-row lg:flex-row gap-4 justify-center items-center cursor-pointer hover:bg-black hover:bg-[url('/public/assets/image-gallery/carbon_BG-20.png')] text-gray-600 hover:text-white p-4 rounded-lg">
+            <div className="">
+              <h1 className="text-2xl text-left font-bold ">
+                Blog of Engine reebuilding
               </h1>
               <p className="text-xl text-justify mt-4">
-                An orifice tube is similar to an expansion valve, with both
-                performing an important function in a vehicle’s A/C system. The
-                valve helps control the flow of refrigerant through the
-                evaporator
+                <div className="collapse">
+                  <input type="checkbox" />
+                  <div className="collapse-title">
+                    At a certain time in your life, especially if you are a car
+                    guy or gal, you may find the need to rebuild an engine, and
+                    there are many reasons why you might discover this. You may
+                    want your engine to perform like
+                    <br />
+                    <span className="text-red-500">Read More</span>
+                  </div>
+                  <div className="collapse-content">
+                    <p>
+                      <span className="text-2xl font-bold">
+                        Blog Engine reebuilding?
+                      </span>
+                      <br />
+                      or better than, the day it rolled out from the factory or
+                      maybe you are just curious about what actually happens
+                      inside of an engine. Whatever your reasons are for tearing
+                      apart your engine you still might wonder how you should go
+                      about disassembling your engine. If you have never torn
+                      apart an engine before
+                      <br />
+                      <br />
+                      It is used to gather clinical information necessary for
+                      making a diagnosis. The scope of diagnostic test ranges
+                      from signs and symptoms elicited during imaging tests,
+                      clinical examination, to pathologic,
+                      <br />
+                      <br />
+                      <span className="text-2xl font-bold">
+                        Transmissin working
+                      </span>
+                      <br />
+                      <br />
+                      biochemical, and psychological tests. Getting the right
+                      diagnosis is a crucial aspect of health care or clinical
+                      practice as it provides an explanation of a patient's
+                      health problem and informs whether a patient has or does
+                      not have a condition. Adequate degree of reliability and
+                      validity are the most basic parameters that need to be
+                      established about any clinical test.
+                    </p>
+                  </div>
+                </div>
               </p>
             </div>
             <img
@@ -122,15 +256,57 @@ const BlogService = () => {
           </div>
           <hr className="text-2xl border-red-400 border-b-2 mb-12" />
 
-          <div className="mb-10 flex flex-col-reverse md:flex-row lg:flex-row gap-4 justify-center items-center cursor-pointer hover:bg-black hover:bg-[url('/public/assets/image-gallery/carbon_BG-20.png')] text-gray-600 hover:text-white p-4">
-            <div>
-              <h1 className="text-2xl text-center font-bold ">
-                Blog A/C Components Description
+          <div className="mb-10 flex flex-col-reverse md:flex-row lg:flex-row gap-4 justify-center items-center cursor-pointer hover:bg-black hover:bg-[url('/public/assets/image-gallery/carbon_BG-20.png')] text-gray-600 hover:text-white p-4 rounded-lg">
+            <div className="">
+              <h1 className="text-2xl text-left font-bold ">
+                Blog of Filter replacement
               </h1>
               <p className="text-xl text-justify mt-4">
-                To keep our environment to our desired temperature, the air
-                conditioning system is the device that helps us achieve that.
-                The system is so common
+                <div className="collapse">
+                  <input type="checkbox" />
+                  <div className="collapse-title">
+                    At a certain time in your life, especially if you are a car
+                    guy or gal, you may find the need to rebuild an engine, and
+                    there are many reasons why you might discover this. You may
+                    want your engine to perform like
+                    <br />
+                    <span className="text-red-500">Read More</span>
+                  </div>
+                  <div className="collapse-content">
+                    <p>
+                      <span className="text-2xl font-bold">
+                        Blog Filter replacement
+                      </span>
+                      <br />
+                      or better than, the day it rolled out from the factory or
+                      maybe you are just curious about what actually happens
+                      inside of an engine. Whatever your reasons are for tearing
+                      apart your engine you still might wonder how you should go
+                      about disassembling your engine. If you have never torn
+                      apart an engine before
+                      <br />
+                      <br />
+                      It is used to gather clinical information necessary for
+                      making a diagnosis. The scope of diagnostic test ranges
+                      from signs and symptoms elicited during imaging tests,
+                      clinical examination, to pathologic,
+                      <br />
+                      <br />
+                      <span className="text-2xl font-bold">
+                        Filter replacement
+                      </span>
+                      <br />
+                      <br />
+                      biochemical, and psychological tests. Getting the right
+                      diagnosis is a crucial aspect of health care or clinical
+                      practice as it provides an explanation of a patient's
+                      health problem and informs whether a patient has or does
+                      not have a condition. Adequate degree of reliability and
+                      validity are the most basic parameters that need to be
+                      established about any clinical test.
+                    </p>
+                  </div>
+                </div>
               </p>
             </div>
             <img
