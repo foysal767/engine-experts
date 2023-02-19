4cd48c3c6f5a6a29f9f84b5ba4f1cead59b1de78
Km-Nazrul-Islam <nazrulislamrongon@gmail.com>
2023-02-19 21:17:00 +0600

    Banner Resposive Added

--- src/Pages/Blog/BlogBanner/BlogBanner.tsx
+++ src/Pages/Blog/BlogBanner/BlogBanner.tsx
@@ -2,8 +2,10 @@ const BlogBanner = () => {
   return (
     <section className="bg-black bg-[url('/public/assets/image-gallery/carbon_BG-20.png')] h-6/12">
       <div className="bannerWrapper flex flex-col md:flex-row lg:flex-row p-16 gap-8">
-        <div className="leftBanner basis-6/12">
-          <div className="text-4xl font-extrabold">Engine Expertise Blog</div>
+        <div className="leftBanner basis-6/12 justify-center items-center">
+          <div className="text-4xl font-extrabold md:mt-32 lg:mt-60">
+            Engine Expertise Blog
+          </div>
         </div>
         <div className="leftBanner basis-6/12">
           <img src="assets/blog/bannarBlog6.png" alt="" />
